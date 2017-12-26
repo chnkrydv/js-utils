c8d41a72244374cc3cb0b4c464f8e595b7bf85ed
chandanktricon <chn291289@gmail.com>
2017-12-26 19:11:32 +0530

    search v1.2

--- search/search.js
+++ search/search.js
@@ -2,11 +2,9 @@ const search = (list, searchables, queryString) => {
   let matchingItemsList = list;
 
   matchingItemsList = matchingItemsList.filter( item => {
-    let match = searchables.some(searchableField => {
+    return searchables.some(searchableField => {
       return item[searchableField].toLowerCase().includes(queryString);
     });
-    
-    return match;
   });
 
   return matchingItemsList;
