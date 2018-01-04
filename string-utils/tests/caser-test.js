ff6dc5e9de9a714aa6a2eda6a029bae5d377c0cd
Chandan Kumar <chn291289@gmail.com>
2018-01-04 18:34:35 +0530

    caser v0

---
+++ string-utils/tests/caser-test.js
@@ -0,0 +1,7 @@
+require('../caser');
+
+
+console.log('audhiudhuiuhd'.firstCap());
+console.log('KUDHHDKD'.firstSmall());
+console.log(' kdjhud dcjo c  jp    wpjkpoopk cpoc wpe powjeopdfj   '.allFirstCap());
+console.log('  JY JEED JE EJ   EDCV D  DWD   EDI TKTGG   ERG JK   ELND LE       '.allFirstSmall());
